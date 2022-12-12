b354920d1ac40932ae424ffb0ce33a391803f758
Youssef Elaissaoui <yelaissa@c1r3p6.1337.ma>
2022-12-12 20:27:02 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Acceuil from './components/Acceuil'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Acceuil />
+    </div>
   )
 }
---
+++ src/components/Acceuil.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Acceuil() {
+  return (
+	<div>Acceuil</div>
+  )
+}
